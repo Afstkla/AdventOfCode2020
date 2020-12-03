5e7dee3d28dd6fd918097233e2a77a136a663068
Job <job@amber.team>
2020-12-03 10:26:14 +0100

    Added new day3 solution

--- AoC2020/Day3.swift
+++ AoC2020/Day3.swift
@@ -10,48 +10,31 @@ import Foundation
 class Day3: Day {
     override var dayIndex: Int { get { 3 } set {}}
     
-    var puzzleGrid = [[String]]()
+    var puzzleGrid = [String]()
     
     func makePuzzleGrid() {
-        puzzleGrid = [[String]]()
-        
-        for line in inputString.split(separator: "\n") {
-            var puzzleLine = [String]()
-            for i in 0..<line.count {
-                puzzleLine.append(String(line[i]))
-            }
-            puzzleGrid.append(puzzleLine)
-        }
+        puzzleGrid = inputString.split(separator: "\n").map { String($0) }
     }
 
     override func part1() -> String {
-        makePuzzleGrid()
-        
-        var treeCounter = 0
-        for i in 0..<puzzleGrid.count {
-            if puzzleGrid[i][3 * i % puzzleGrid[i].count] == "#" {
-                treeCounter += 1
-            }
-        }
-        
-        return "\(treeCounter)"
+        return "\(puzzleGrid.indices.filter { puzzleGrid[$0][($0 * 3) % puzzleGrid[$0].count] == "#" }.count)"
     }
     
     override func part2() -> String {
-        var multiplier = 1
-        for movementOption in [(1,1), (3,1), (5,1), (7,1), (1,2)] {
-            var treeCounter = 0
-            for i in 0..<puzzleGrid.count {
-                if i * movementOption.1 >= puzzleGrid.count {
-                    break
-                }
-                if puzzleGrid[i * movementOption.1][(movementOption.0 * i) % puzzleGrid[i].count] == "#" {
-                    treeCounter += 1
-                }
+        let multiplier = ([(1,1), (3,1), (5,1), (7,1), (1,2)].reduce(1) { (result, newValue) in
+            let trees = puzzleGrid.indices.filter { (idx) in
+                let somehowIfIDontUseThisVariableTheCompilerComplains = (idx % newValue.1) == 0 && puzzleGrid[idx][((idx / newValue.1) * newValue.0) % puzzleGrid[idx].count] == "#"
+                return somehowIfIDontUseThisVariableTheCompilerComplains
             }
-            multiplier *= treeCounter
-        }
-        
+            
+            return result * trees.count
+        })
         return "\(multiplier)"
+    }
+    
+    override func solve() {
+        makePuzzleGrid()
+        
+        super.solve()
     }
 }
