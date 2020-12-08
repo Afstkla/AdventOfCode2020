1a4bf45b59383caf557bc8396f87abe0e0358e9c
Job <job@amber.team>
2020-12-08 09:34:11 +0100

    Prepared day9 and added solutions in comments

---
+++ AoC2020/Days/Day9.swift
@@ -0,0 +1,24 @@
+//
+//  Day9.swift
+//  AoC2020
+//
+//  Created by Job Nijenhuis on 08/12/2020.
+//
+
+import Foundation
+
+class Day9: Day {
+    override var dayIndex: Int { get { 9 } set {}}
+    
+    override func prepareInput() {
+        
+    }
+    
+    override func part1() -> String {
+        ""
+    }
+    
+    override func part2() -> String {
+        ""
+    }
+}
--- AoC2020/main.swift
+++ AoC2020/main.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-//Day1(inputFile: "input_day1").solve()
-//Day2(inputFile: "input_day2").solve()
-//Day3(inputFile: "input-3").solve()
-//Day4(inputFile: "input-4").solve()
-//Day5(inputFile: "input-5").solve()
-//Day6(inputFile: "input-6").solve()
-//Day7(inputFile: "input-7").solve()
-Day8(inputFile: "input-8").solve()
+//Day1(inputFile: "input_day1").solve()   // 211899,      275765682
+//Day2(inputFile: "input_day2").solve()   // 600,         245
+//Day3(inputFile: "input-3").solve()      // 294,         5774564250
+//Day4(inputFile: "input-4").solve()      // 182,         109
+//Day5(inputFile: "input-5").solve()      // 832,         517
+//Day6(inputFile: "input-6").solve()      // 6703,        3430
+//Day7(inputFile: "input-7").solve()      // 337,         50100
+//Day8(inputFile: "input-8").solve()      // 1394,        1626
+Day9(inputFile: "input-9").solve()
