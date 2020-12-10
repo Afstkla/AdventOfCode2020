1f581440a80f8298a918cf53a7d98ff86d640e87
Job <job@amber.team>
2020-12-10 12:04:12 +0100

    Slight refactor of day 10, added reasoning in comments

--- AoC2020/Days/Day10.swift
+++ AoC2020/Days/Day10.swift
@@ -59,39 +59,32 @@ class Day10: Day {
     """
     
     var numbers = [Int]()
+    var differences = [Int]()
     
     override func prepareInput() {
         numbers = [0] + inputString.components(separatedBy: .newlines).compactMap(Int.init).sorted(by: <)
         numbers.append(numbers.max()! + 3)
+        
+        differences = numbers[1..<numbers.count].enumerated().map {
+            // Differences
+            $1 - numbers[$0]
+        }
     }
     
     override func part1() -> String {
-        var oneJoltDiff = 0
-        var threeJoltDiff = 0
-        
-        for i in 1..<numbers.count {
-            if numbers[i] - numbers[i - 1] == 1 {
-                oneJoltDiff += 1
-            } else if numbers[i] - numbers[i - 1] == 3 {
-                threeJoltDiff += 1
-            }
-        }
-        
-        return String(oneJoltDiff * threeJoltDiff)
+        String(differences.filter { $0 == 1 }.count * differences.filter { $0 == 3 }.count)
     }
     
     override func part2() -> String {
-        let splits: Int = numbers[1..<numbers.count].enumerated().map {
-            // Differences
-            $1 - numbers[$0]
-        }.split(separator: 3).map {
+        String(differences.split(separator: 3).map {
             // Split by tiny jumps
-            $0.count - 1
-        }.reduce(1) { res, newValue in
-            let somehowIfIDontUseThisVariableTheCompilerComplains = Int(pow(2, Double(newValue)) - floor(pow(3, Double(newValue - 3))))
-            return somehowIfIDontUseThisVariableTheCompilerComplains * res
-        }
-        
-        return String(splits)
+            $0.count
+        }.reduce(1) { intermediateResult, newValue in
+            // We have a list of numbers 0, 1, 2, ..., n, n + 3
+            // Possible arrangements are 2^(n - 1) if the maximum jump size wouldn't be 3. With the maximum jump size 3, it starts to affect the result starting at n = 4 (as we can't remove all 1..<n-1 values, as that'd give a jump of 4)
+            // To solve this, we have to adjust for the number of invalid jumps, which is given by 3^(n - 4) for any value of n >= 4. To solve n < 3, we use the floor function to reduce the value to 0
+            let possibleCombinations = Int(pow(2, Double(newValue - 1)) - floor(pow(3, Double(newValue - 4))))
+            return possibleCombinations * intermediateResult
+        })
     }
 }
