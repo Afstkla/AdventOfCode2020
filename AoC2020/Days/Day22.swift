ccd80fdb1c8b82d4c2e4f6999cc31fcbe164455b
Job <job@amber.team>
2020-12-22 08:06:47 +0100

    Day22

--- AoC2020/Days/Day22.swift
+++ AoC2020/Days/Day22.swift
@@ -10,15 +10,90 @@ import Foundation
 class Day22: Day {
     override var dayIndex: Int { get { 22 } set {} }
     
+    var playerOneDeck: [Int] = []
+    var playerTwoDeck: [Int] = []
+    
     override func prepareInput() {
+        let decks = inputString.trimmed().components(separatedBy: "\n\n")
         
+        playerOneDeck = decks[0].components(separatedBy: .newlines)[1...].map { Int($0)! }
+        playerTwoDeck = decks[1].components(separatedBy: .newlines)[1...].map { Int($0)! }
+    }
+    
+    enum Winner {
+        case playerOne
+        case playerTwo
+    }
+    
+    func playGame(deckOne: [Int], deckTwo: [Int]) -> (winner: Winner, deck: [Int]) {
+        var localDeckOne = deckOne
+        var localDeckTwo = deckTwo
+        
+        var previousRoundsDeckOne: [[Int]] = []
+        var previousRoundsDeckTwo: [[Int]] = []
+        
+        while !localDeckOne.isEmpty && !localDeckTwo.isEmpty {
+            if previousRoundsDeckOne.firstIndex(of: localDeckOne) == previousRoundsDeckTwo.firstIndex(of: localDeckTwo) && previousRoundsDeckOne.firstIndex(of: localDeckOne) != nil {
+                return (winner: .playerOne, deck: playerOneDeck)
+            } else {
+                previousRoundsDeckOne.append(localDeckOne)
+                previousRoundsDeckTwo.append(localDeckTwo)
+            }
+
+            if playRound(deckOne: localDeckOne, deckTwo: localDeckTwo) == .playerOne {
+                localDeckOne.append(contentsOf: [localDeckOne[0], localDeckTwo[0]])
+            } else {
+                localDeckTwo.append(contentsOf: [localDeckTwo[0], localDeckOne[0]])
+            }
+            
+            localDeckOne.removeFirst()
+            localDeckTwo.removeFirst()
+        }
+        
+        return localDeckOne.isEmpty ? (winner: .playerTwo, deck: localDeckTwo) : (winner: .playerOne, deck: localDeckOne)
+    }
+    
+    func playRound(deckOne: [Int], deckTwo: [Int]) -> Winner {
+        var localDeckOne = deckOne
+        var localDeckTwo = deckTwo
+        
+        let playerOneCard = localDeckOne.removeFirst()
+        let playerTwoCard = localDeckTwo.removeFirst()
+        
+        if playerOneCard <= localDeckOne.count && playerTwoCard <= localDeckTwo.count {
+            return playGame(deckOne: [Int](localDeckOne[0..<playerOneCard]), deckTwo: [Int](localDeckTwo[0..<playerTwoCard])).winner
+        }
+        
+        if playerOneCard > playerTwoCard {
+            return .playerOne
+        } else {
+            return .playerTwo
+        }
     }
     
     override func part1() -> String {
-        ""
+        var localPlayerOneDeck = playerOneDeck
+        var localPlayerTwoDeck = playerTwoDeck
+        
+        while !localPlayerOneDeck.isEmpty && !localPlayerTwoDeck.isEmpty {
+            let playerOneCard = localPlayerOneDeck.removeFirst()
+            let playerTwoCard = localPlayerTwoDeck.removeFirst()
+            
+            if playerOneCard > playerTwoCard {
+                localPlayerOneDeck.append(contentsOf: [playerOneCard, playerTwoCard])
+            } else {
+                localPlayerTwoDeck.append(contentsOf: [playerTwoCard, playerOneCard])
+            }
+        }
+        
+        let deckToReturn = localPlayerOneDeck.isEmpty ? localPlayerTwoDeck : localPlayerOneDeck
+        
+        return String(deckToReturn.reversed().enumerated().reduce(0) { $0 + $1.element * ($1.offset + 1) })
     }
     
     override func part2() -> String {
-        ""
+        let winner = playGame(deckOne: playerOneDeck, deckTwo: playerTwoDeck)
+                        
+        return String(winner.deck.reversed().enumerated().reduce(0) { $0 + $1.element * ($1.offset + 1) })
     }
 }
--- AoC2020/main.swift
+++ AoC2020/main.swift
@@ -28,8 +28,8 @@ let startTime = Date()
 //Day18(inputFile: "input-18").solve()    // 8298263963837,     145575710203332
 //Day19(inputFile: "input-19").solve()    // 285,               411
 //Day20(inputFile: "input-20").solve()    // 5775714912743,     1836
-Day21(inputFile: "input-21").solve()    //
-//Day22(inputFile: "input-22").solve()    //
+//Day21(inputFile: "input-21").solve()    // 2423,              jzzjz,bxkrd,pllzxb,gjddl,xfqnss,dzkb,vspv,dxvsp
+Day22(inputFile: "input-22").solve()    // 33473,             31793
 //Day23(inputFile: "input-23").solve()    //
 //Day24(inputFile: "input-24").solve()    //
 //Day25(inputFile: "input-25").solve()    //
